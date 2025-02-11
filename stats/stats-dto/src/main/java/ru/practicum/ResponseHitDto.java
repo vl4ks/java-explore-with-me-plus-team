a80e7cd3a02e00354a666d818ba331757b2fa3d6
Andrey Didan <andrey_didan@mail.ru>
2025-02-12 00:31:36 +0300

    Добавил модули, добавил DTO, приблизительно построил директории в сервисе, сделал схему из одной таблицы и пока предварительный файл application.properties

---
+++ stats/stats-dto/src/main/java/ru/practicum/ResponseHitDto.java
@@ -0,0 +1,22 @@
+package ru.practicum;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResponseHitDto {
+    private Long id;
+
+    private String app;
+
+    private String url;
+
+    private String ip;
+
+    private String timestamp;
+}
