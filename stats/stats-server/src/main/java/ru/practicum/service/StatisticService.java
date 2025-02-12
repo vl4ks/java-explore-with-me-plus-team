4a47123c023ba71f95c5c287388b9ea6a6a567b7
Andrey Didan <andrey_didan@mail.ru>
2025-02-12 05:24:08 +0300

    Поправил немного чекстайл

--- stats/stats-server/src/main/java/ru/practicum/service/StatisticService.java
+++ stats/stats-server/src/main/java/ru/practicum/service/StatisticService.java
@@ -4,11 +4,10 @@ import ru.practicum.CreateHitDto;
 import ru.practicum.ResponseHitDto;
 import ru.practicum.CreateStatsDto;
 import ru.practicum.ResonseStatsDto;
-
 import java.util.List;
 
 public interface StatisticService {
-
     ResponseHitDto create(CreateHitDto createHitDto);
+
     List<ResonseStatsDto> get(CreateStatsDto createStatsDto);
 }
