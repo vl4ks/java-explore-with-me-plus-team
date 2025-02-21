efb818f10903cf49466c8873a505e1b77fea7bfd
vl4ks <veli4koksenia@yandex.ru>
2025-02-21 17:08:00 +0500

    пробую исправить ошибки в check and build application

--- main-service/src/main/java/ru/practicum/event/dto/EventRequestStatusUpdateResult.java
+++ main-service/src/main/java/ru/practicum/event/dto/EventRequestStatusUpdateResult.java
@@ -3,12 +3,10 @@ package ru.practicum.event.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 public class EventRequestStatusUpdateResult {
 
 }
--- main-service/src/main/java/ru/practicum/event/model/Event.java
+++ main-service/src/main/java/ru/practicum/event/model/Event.java
@@ -4,13 +4,11 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Entity
 @Builder
 @AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "events")
 public class Event {
 }
