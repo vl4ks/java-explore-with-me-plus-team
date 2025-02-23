4353f60cc1ab15667e492feb1b026aede2030e5e
MariaFantasy <peggy-sju@ya.ru>
2025-02-23 19:51:08 +0300

    Добавление обработки ошибки в запросе /users/{userId}/requests

--- main-service/src/main/java/ru/practicum/request/service/EventRequestServiceImpl.java
+++ main-service/src/main/java/ru/practicum/request/service/EventRequestServiceImpl.java
@@ -10,6 +10,8 @@ import ru.practicum.request.mapper.EventRequestDtoMapper;
 import ru.practicum.request.model.EventRequest;
 import ru.practicum.request.model.EventRequestStatus;
 import ru.practicum.request.storage.EventRequestRepository;
+import ru.practicum.user.dto.UserDto;
+import ru.practicum.user.service.UserService;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -19,9 +21,9 @@ import java.util.stream.Collectors;
 @Service("eventRequestServiceImpl")
 @RequiredArgsConstructor
 public class EventRequestServiceImpl implements EventRequestService {
+    private final UserService userService;
     private final EventRequestRepository eventRequestRepository;
     private final EventRequestDtoMapper eventRequestDtoMapper;
-
 
     private EventRequest findByRequesterAndEvent(Long requesterId, Long eventId) {
         final EventRequest request = eventRequestRepository.findByRequesterAndEvent(requesterId, eventId);
@@ -73,6 +75,9 @@ public class EventRequestServiceImpl implements EventRequestService {
 
     @Override
     public Collection<ParticipationRequestDto> getByRequesterId(Long requesterId) {
+        if (userService.findById(requesterId) == null) {
+            throw new NotFoundException("User with id=" + requesterId + " was not found");
+        }
         final Collection<EventRequest> requests = eventRequestRepository.findByRequesterId(requesterId);
         return requests.stream()
                 .map(eventRequestDtoMapper::mapToResponseDto)
