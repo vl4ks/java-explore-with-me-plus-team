da0cc2f2a967a3d02f847df730ad085daed8f2e8
MariaFantasy <peggy-sju@ya.ru>
2025-02-25 06:59:55 +0300

    Удаление лишних библиотек

--- main-service/src/main/java/ru/practicum/category/controller/AdminCategoryController.java
+++ main-service/src/main/java/ru/practicum/category/controller/AdminCategoryController.java
@@ -2,7 +2,6 @@ package ru.practicum.category.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
