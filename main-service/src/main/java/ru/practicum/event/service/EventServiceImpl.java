6d636b2084780618b2087c707ce2ac6d89027f2e
MariaFantasy <peggy-sju@ya.ru>
2025-02-28 19:37:57 +0300

    Корректировка checkstyle

--- main-service/src/main/java/ru/practicum/event/service/EventServiceImpl.java
+++ main-service/src/main/java/ru/practicum/event/service/EventServiceImpl.java
@@ -1,7 +1,6 @@
 package ru.practicum.event.service;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
