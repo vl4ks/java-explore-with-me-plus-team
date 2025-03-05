336d7bdabdbe4effe76d447e6afe42672a701bdb
MariaFantasy <peggy-sju@ya.ru>
2025-03-06 00:50:34 +0300

    Удаление лишней библиотеки

--- main-service/src/main/java/ru/practicum/event/controller/PublicController.java
+++ main-service/src/main/java/ru/practicum/event/controller/PublicController.java
@@ -1,6 +1,5 @@
 package ru.practicum.event.controller;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
