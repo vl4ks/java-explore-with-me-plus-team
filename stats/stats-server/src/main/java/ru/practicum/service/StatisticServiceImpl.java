bdcf21271cfc99a3586358fda764cad606badab7
MariaFantasy <peggy-sju@ya.ru>
2025-03-06 15:37:34 +0300

    Update StatisticServiceImpl.java

--- stats/stats-server/src/main/java/ru/practicum/service/StatisticServiceImpl.java
+++ stats/stats-server/src/main/java/ru/practicum/service/StatisticServiceImpl.java
@@ -11,8 +11,6 @@ import ru.practicum.mapper.HitMapper;
 import ru.practicum.model.Hit;
 import ru.practicum.reposirory.StatisticRepository;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -36,9 +34,6 @@ public class StatisticServiceImpl implements StatisticService {
         if (start == null || end == null) {
             throw new ValidationException("You need to chose start and end dates.");
         }
-        final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        final LocalDateTime startDateTime = LocalDateTime.parse(start, formatter);
-        final LocalDateTime endDateTime = LocalDateTime.parse(end, formatter);
 
         if (start.compareTo(end) > 0) {
             throw new ValidationException("Дата начала должна быть раньше даты окончания.");
@@ -46,9 +41,9 @@ public class StatisticServiceImpl implements StatisticService {
 
         final List<StatisticRepository.ResponseStatsDto> hits;
         if (uris.isEmpty()) {
-            hits = statisticRepository.getByAllUris(startDateTime, endDateTime);
+            hits = statisticRepository.getByAllUris(start, end);
         } else {
-            hits = statisticRepository.getByUris(startDateTime, endDateTime, uris);
+            hits = statisticRepository.getByUris(start, end, uris);
         }
         return hits.stream()
                 .map(stats -> ResponseStatsDto.builder()
